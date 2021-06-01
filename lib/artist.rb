8ba484c64349506a890e80caa3f7648758a235a8
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-06-01 03:10:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 class Artist
   attr_accessor :name, :songs
   @@all=[]
+  
   def initialize(name)
     @name=name
     @@all<<self
@@ -15,10 +16,7 @@ class Artist
   end
 
   def new_song(name, genre)
-    song=Song.new
-    song.name=name
-    song.genre=genre
-    song.artist=self
+    song=Song.new(name,self,genre)
   end
 
 
